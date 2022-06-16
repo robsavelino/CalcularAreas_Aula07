7c939b49a3a350743d8177f2e1f3c850c6c0c009
Roberto Avelino <roberto.avelino97@hotmail.com>
2022-06-15 21:35:37 -0300

    Add project files.

---
+++ ConsoleApp1/Formulas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaAreas
+{
+    public class Quadrado : Forma
+    {
+        public double Lado { get; set; }
+
+        public override void Area()
+        {
+            Console.WriteLine($"A área do quadrado de lado {Lado} é: {Lado * Lado}");
+        }
+    }
+
+    public class Retangulo : Forma
+    {
+        public double Base { get; set; }
+
+        public double Altura { get; set; }
+
+        public override void Area()
+        {
+            Console.WriteLine($"A área do retangulo de base {Base} e altura {Altura} é: {(Base * Altura)}");
+        }
+
+    }
+
+
+    public class Triangulo : Forma
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public override void Area()
+        {
+            Console.WriteLine($"A área do triangulo de base {Base} e altura {Altura} é: {(Base * Altura)/2}");
+        }
+    }
+
+    public class Circulo : Forma
+    {
+        public double Raio { get; set; }
+
+        public override void Area()
+        {
+            Console.WriteLine($"A área do circuo de raio {Raio} é: {Math.Pow(Raio, 2)* Math.PI}");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using CalculaAreas;
+
+Console.WriteLine("Este programa calcula as áreas de diversas formas geométricas");
+
+
+Console.WriteLine("--------------------------------------------------------");
+Console.WriteLine("Calculando a área do Quadrado");
+Quadrado quadrado = new Quadrado();
+quadrado.Lado = 5;
+Console.WriteLine();
+quadrado.Area();
+
+
+Console.WriteLine();
+Console.WriteLine("--------------------------------------------------------");
+Console.WriteLine("Calculando a área do Retângulo");
+Retangulo retangulo = new Retangulo();
+retangulo.Base = 5;
+retangulo.Altura = 3;
+retangulo.Area();
+
+Console.WriteLine();
+Console.WriteLine("--------------------------------------------------------");
+Console.WriteLine("Calculando a área do Triângulo");
+Triangulo triangulo = new Triangulo();
+triangulo.Base = 4;
+triangulo.Altura = 3;
+triangulo.Area();
+
+Console.WriteLine();
+Console.WriteLine("--------------------------------------------------------");
+Console.WriteLine("Calculando a área do Circulo");
+Circulo circulo = new Circulo();
+circulo.Raio = 10;
+circulo.Area();
+
+Console.ReadKey();
+
+
+
+
